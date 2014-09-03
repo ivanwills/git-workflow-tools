cf32b369d53b476e92c734c66774a4074dfdd5f0
Ivan Wills <ivan.wills@gmail.com>
2014-09-03 17:43:11 +1000

    Changed to make all extra sha_from_show optional

--- lib/Git/Workflow.pm
+++ lib/Git/Workflow.pm
@@ -169,7 +169,7 @@ sub releases {
 }
 
 sub sha_from_show {
-    my ($name, $no_branches) = @_;
+    my ($name, %options) = @_;
     my ($log) = runner("git rev-list -1 --timestamp $name");
     chomp $log;
     my ($time, $sha) = split /\s+/, $log;
@@ -178,8 +178,22 @@ sub sha_from_show {
         name     => $name,
         sha      => $sha,
         time     => $time,
-        branches => $no_branches ? {} : { map { $_ => 1 } branches('both', $sha) },
+        branches => $options{branches} ? { map { $_ => 1 } branches('both', $sha) } : {},
+        files    => $options{files}    ? files_from_sha($sha) : {},
     };
+}
+
+sub files_from_sha {
+    my ($sha) = @_;
+    my $show = $git->show('--name-status', $sha);
+    $show =~ s/\A.*\n\n//xms;
+    my %files;
+    for my $file (split /\n/, $show) {
+        my ($state, $file) = split /\s+/, $file, 2;
+        $files{$file} = $state;
+    }
+
+    return \%files;
 }
 
 sub slurp {
