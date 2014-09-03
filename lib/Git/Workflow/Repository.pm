44cb067d1c949cfe5a8e6bc70e64f9682af56096
Ivan Wills <ivan.wills@gmail.com>
2014-09-04 08:09:09 +1000

    Cleaned up documentation

--- lib/Git/Workflow/Repository.pm
+++ lib/Git/Workflow/Repository.pm
@@ -62,93 +62,50 @@ __END__
 
 =head1 NAME
 
-Git::Workflow::Repository - <One-line description of module's purpose>
+Git::Workflow::Repository - A basic wrapper around GIT
 
 =head1 VERSION
 
 This documentation refers to Git::Workflow::Repository version 0.3
 
-
 =head1 SYNOPSIS
 
-   use Git::Workflow::Repository;
+   use Git::Workflow::Repository qw/git/;
 
-   # Brief but working code example(s) here showing the most common usage(s)
-   # This section will be as far as many users bother reading, so make it as
-   # educational and exemplary as possible.
+   # get the git object based on the current directory
+   my $git = git();
 
+   # call git sub commands
+   $git->log('--oneline');
+   $git->show('123ABC');
+   $git->status();
 
 =head1 DESCRIPTION
 
-A full description of the module and its features.
-
-May include numerous subsections (i.e., =head2, =head3, etc.).
-
+This is a thin wrapper around L<Git> that will default to using the current
+directory for C<git>, adds a simple way of calling git sub-commands
+as methods.
 
 =head1 SUBROUTINES/METHODS
 
-A separate section listing the public components of the module's interface.
+=head3 C<new (%options)>
 
-These normally consist of either subroutines that may be exported, or methods
-that may be called on objects belonging to the classes that the module
-provides.
+Returns a new Git::Workflow::Repository
 
-Name the section accordingly.
+=head2 C<git ()>
 
-In an object-oriented module, this section should begin with a sentence (of the
-form "An object of this class represents ...") to give the reader a high-level
-context to help them understand the methods that are subsequently described.
-
-
-=head3 C<new ( $search, )>
-
-Param: C<$search> - type (detail) - description
-
-Return: Git::Workflow::Repository -
-
-Description:
-
-=cut
-
+Singleton to get the last created C<Git::Workflow::Repository> object or
+create a new one.
 
 =head1 DIAGNOSTICS
 
-A list of every error and warning message that the module can generate (even
-the ones that will "never happen"), with a full explanation of each problem,
-one or more likely causes, and any suggested remedies.
-
 =head1 CONFIGURATION AND ENVIRONMENT
-
-A full explanation of any configuration system(s) used by the module, including
-the names and locations of any configuration files, and the meaning of any
-environment variables or properties that can be set. These descriptions must
-also include details of any configuration language used.
 
 =head1 DEPENDENCIES
 
-A list of all of the other modules that this module relies upon, including any
-restrictions on versions, and an indication of whether these required modules
-are part of the standard Perl distribution, part of the module's distribution,
-or must be installed separately.
-
 =head1 INCOMPATIBILITIES
 
-A list of any modules that this module cannot be used in conjunction with.
-This may be due to name conflicts in the interface, or competition for system
-or program resources, or due to internal limitations of Perl (for example, many
-modules that use source code filters are mutually incompatible).
-
 =head1 BUGS AND LIMITATIONS
-
-A list of known problems with the module, together with some indication of
-whether they are likely to be fixed in an upcoming release.
-
-Also, a list of restrictions on the features the module does provide: data types
-that cannot be handled, performance issues and the circumstances in which they
-may arise, practical limitations on the size of data sets, special cases that
-are not (yet) handled, etc.
-
-The initial template usually just has:
 
 There are no known bugs in this module.
 
@@ -159,7 +116,6 @@ Patches are welcome.
 =head1 AUTHOR
 
 Ivan Wills - (ivan.wills@gmail.com)
-<Author name(s)>  (<contact address>)
 
 =head1 LICENSE AND COPYRIGHT
 
