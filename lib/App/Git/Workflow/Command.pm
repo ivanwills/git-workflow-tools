6b9fe18c23d956aa31cfb8a3b89cdb2acc76c241
Ivan Wills <ivan.wills@gmail.com>
2014-09-18 20:28:37 +1000

    New helper for moving scripts to modules

---
+++ lib/App/Git/Workflow/Command.pm
@@ -0,0 +1,118 @@
+package App::Git::Workflow::Command;
+
+# Created on: 2014-03-11 20:58:59
+# Create by:  Ivan Wills
+# $Id$
+# $Revision$, $HeadURL$, $Date$
+# $Revision$, $Source$, $Date$
+
+use strict;
+use warnings;
+use Getopt::Long;
+use Pod::Usage ();
+use Data::Dumper qw/Dumper/;
+use English qw/ -no_match_vars /;
+use base qw/Exporter/;
+
+our $VERSION   = 0.6;
+our @EXPORT_OK = qw/get_options/;
+
+sub get_options {
+    my ($option, @options) = @_;
+    my ($caller_package) = caller;
+
+    Getopt::Long::Configure('bundling');
+    GetOptions(
+        $option,
+        'verbose|v+',
+        'man',
+        'help',
+        'VERSION!',
+    ) or Pod::Usage::pod2usage(
+        -verbose => 1,
+        -input   => $caller_package,
+    );
+
+    if ( $option->{'VERSION'} ) {
+        print "${caller_package}::name Version = $VERSION\n";
+        return 1;
+    }
+    elsif ( $option->{'man'} ) {
+        Pod::Usage::pod2usage(
+            -verbose => 2,
+            -input   => $caller_package,
+        ),
+    }
+    elsif ( $option->{'help'} ) {
+        Pod::Usage::pod2usage(
+            -verbose => 1,
+            -input   => $caller_package,
+        ),
+    }
+
+    return;
+}
+
+1;
+
+__DATA__
+
+=head1 NAME
+
+App::Git::Workflow::Command - Helper for other commands
+
+=head1 VERSION
+
+This documentation refers to App::Git::Workflow::Command version 0.6
+
+=head1 SYNOPSIS
+
+   use App::Git::Workflow::Command qw/get_option/;
+
+   my %option;
+   get_options( \%option, 'test|t!', 'quiet|q' );
+
+=head1 DESCRIPTION
+
+Does the boilerplate for other command modules.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 C<get_options ($option_hash, @options)>
+
+Just a wrapper for L<Getopt::Long>'s C<GetOptions> which configures bundling
+and adds verbose, help, man and VERSION options. Also if C<GetOptions> fails
+usage info will be displayed.
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs in this module.
+
+Please report problems to Ivan Wills (ivan.wills@gmail.com).
+
+Patches are welcome.
+
+=head1 AUTHOR
+
+Ivan Wills - (ivan.wills@gmail.com)
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2014 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
+All rights reserved.
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See L<perlartistic>.  This program is
+distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
+
+=cut
