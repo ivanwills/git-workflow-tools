439d42618ebd622f07d66d77f014fb47a08cacf5
Ivan Wills <ivan.wills@gmail.com>
2014-11-19 07:10:25 +1100

    Fixed spelling

--- lib/App/Git/Workflow/Command/Recent.pm
+++ lib/App/Git/Workflow/Command/Recent.pm
@@ -15,7 +15,7 @@ use English qw/ -no_match_vars /;
 use App::Git::Workflow;
 use App::Git::Workflow::Command qw/get_options/;
 
-our $VERSION  = 0.91;
+our $VERSION  = 0.92;
 our $workflow = App::Git::Workflow->new;
 our ($name)   = $PROGRAM_NAME =~ m{^.*/(.*?)$}mxs;
 our %option;
@@ -132,7 +132,7 @@ git-recent - Find what files have been changed recently in a repository
 
 =head1 VERSION
 
-This documentation refers to git-recent version 0.91
+This documentation refers to git-recent version 0.92
 
 =head1 SYNOPSIS
 
@@ -162,7 +162,7 @@ This documentation refers to git-recent version 0.91
 =head1 DESCRIPTION
 
 C<git-recent> finds all files that have been changed in all branches in the
-repository. This allows colaborators to quickly see who is working on what
+repository. This allows collaborators to quickly see who is working on what
 even if it's in a different branch.
 
 =head1 SUBROUTINES/METHODS
