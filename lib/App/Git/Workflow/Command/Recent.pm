7ea5fd244533a96e6d96042e9dd7b0e88b63e440
Ivan Wills <Ivan.Wills2@optus.com.au>
2018-07-29 20:20:11 +1000

    Updating for release of version 1.0.8

--- lib/App/Git/Workflow/Command/Recent.pm
+++ lib/App/Git/Workflow/Command/Recent.pm
@@ -16,7 +16,7 @@ use English qw/ -no_match_vars /;
 use App::Git::Workflow;
 use App::Git::Workflow::Command qw/get_options/;
 
-our $VERSION  = 1.0.7;
+our $VERSION  = 1.0.8;
 our $workflow = App::Git::Workflow->new;
 our ($name)   = $PROGRAM_NAME =~ m{^.*/(.*?)$}mxs;
 our %option;
@@ -231,7 +231,7 @@ git-recent - Find what files have been changed recently in a repository
 
 =head1 VERSION
 
-This documentation refers to git-recent version 1.0.7
+This documentation refers to git-recent version 1.0.8
 
 =head1 SYNOPSIS
 
