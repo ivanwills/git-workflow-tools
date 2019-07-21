c5dc347bab46d6650dcfec15c3bed481f54b765f
Ivan Wills <Ivan.Wills2@optus.com.au>
2019-07-21 19:40:36 +1000

    Updating for release of version 1.1.5

--- lib/App/Git/Workflow/Command/BranchGrep.pm
+++ lib/App/Git/Workflow/Command/BranchGrep.pm
@@ -14,7 +14,7 @@ use Term::ANSIColor qw/colored/;
 use App::Git::Workflow;
 use App::Git::Workflow::Command qw/get_options/;
 
-our $VERSION  = version->new(1.1.4);
+our $VERSION  = version->new(1.1.5);
 our $workflow = App::Git::Workflow->new;
 our ($name)   = $PROGRAM_NAME =~ m{^.*/(.*?)$}mxs;
 our %option;
@@ -61,7 +61,7 @@ git-branch-grep - grep for branch names
 
 =head1 VERSION
 
-This documentation refers to git-branch-grep version 1.1.4
+This documentation refers to git-branch-grep version 1.1.5
 
 =head1 SYNOPSIS
 
